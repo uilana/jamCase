74c275b382169f55c8a9948259a3277194329ece
uliana <lvovt908@gmail.com>
2021-09-27 18:59:49 +0300

    slider commit

--- app/js/slider.js
+++ app/js/slider.js
@@ -4,8 +4,39 @@ $(document).on('ready', function() {
         infinite: true,
         autoplay: true,
         autoplaySpeed: 2000,
-        slidesToShow: 10,
+        slidesToShow: 9,
         slidesToScroll: 1,
-    });
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 7,
+                    slidesToScroll: 1,
+                },
 
-});
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 1,
+                },
+
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                },
+
+            }, {
+                breakpoint: 490,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+
+            }
+        ]
+    })
+})
